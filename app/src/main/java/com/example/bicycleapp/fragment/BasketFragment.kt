4ac1ee9c3661223a91ffa79a2284d3b08edbe1af
dodoizgi <45079943+dodoizgi@users.noreply.github.com>
2023-03-20 20:31:52 +0300

    Rent pop-up is added. And BasketFragment.kt is fixed.

--- app/src/main/java/com/example/bicycleapp/fragment/BasketFragment.kt
+++ app/src/main/java/com/example/bicycleapp/fragment/BasketFragment.kt
@@ -1,5 +1,6 @@
 package com.example.bicycleapp.fragment
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.bicycleapp.R
 import com.example.bicycleapp.adapter.BasketBicycleListAdapter
 import com.example.bicycleapp.data.SharedPreference
@@ -16,6 +18,9 @@ class BasketFragment : Fragment() {
 
     private var _binding: FragmentBasketBinding? = null
     private val binding get() = _binding!!
+    private var sharedPreference : SharedPreference? = null
+    private lateinit var adapter : BasketBicycleListAdapter
+    private lateinit var recyclerView : RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,12 +35,12 @@ class BasketFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val sharedPreference = SharedPreference(view.context)
-        val recyclerView = binding.basketRecyclerView
+        sharedPreference = SharedPreference(view.context)
+        recyclerView = binding.basketRecyclerView
         recyclerView.layoutManager = LinearLayoutManager(context)
 
-        var adapter =
-            BasketBicycleListAdapter(sharedPreference.getBasket("ORDER"))
+        adapter =
+            BasketBicycleListAdapter(sharedPreference!!.getBasket("ORDER"))
 
         recyclerView.adapter= adapter
 
@@ -43,12 +48,28 @@ class BasketFragment : Fragment() {
             findNavController().navigate(R.id.action_BasketFragment_to_BicycleRentalFragment)
         }
 
-        binding.deleteButton.setOnClickListener {
-            sharedPreference.clearBasket()
-            adapter =
-                BasketBicycleListAdapter(sharedPreference.getBasket("ORDER"))
-            recyclerView.adapter= adapter
+        binding.deleteButton.setOnClickListener { deleteBasket() }
+        binding.basketRentButton.setOnClickListener { dialogYesOrNo() }
+    }
+
+    private fun dialogYesOrNo() {
+        val builder = AlertDialog.Builder(activity)
+        builder.setPositiveButton("Yes") { dialog, id ->
+            dialog.dismiss()
+            deleteBasket()
         }
+        builder.setNegativeButton("No", null)
+        val alert = builder.create()
+        alert.setMessage("Are you sure you want to rent the  bikes?")
+        alert.show()
+    }
+
+    private fun deleteBasket() {
+        sharedPreference?.clearBasket()
+        adapter =
+            BasketBicycleListAdapter(sharedPreference?.getBasket("ORDER"))
+        recyclerView.adapter= adapter
+
     }
 
     override fun onDestroyView() {
